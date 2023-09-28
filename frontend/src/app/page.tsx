92bc177172de74205dcd531999f5aed9efbee7d4
YOON-CC <whdbscks77@gmail.com>
2023-09-29 00:40:04 +0900

    nextjs 설치 및 main 페이지 생성

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,484 @@
+'use client'
+
+import React, { useState, useEffect, useRef  } from 'react';
+// import { Link, useNavigate } from "react-router-dom";
+import styled from 'styled-components';
+import Header from '@/components/Header';
+// import appStore from '../store/appStore';
+import { Element, animateScroll as scroll } from 'react-scroll'; 
+
+interface AnimatedBoxProps {
+    isVisible: boolean;
+}
+
+const Main: React.FC = () => {
+    const [isVisible1, setIsVisible1] = useState(false);
+    const [isVisible2, setIsVisible2] = useState(false);
+    const [isVisible3, setIsVisible3] = useState(false);
+    const targetRef1 = useRef<HTMLDivElement | null>(null);
+    const targetRef2 = useRef<HTMLDivElement | null>(null);
+    const targetRef3 = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        const options = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.5, // Change threshold to trigger animation when partially visible
+        };
+
+        const handleIntersect1: IntersectionObserverCallback = (entries) => {
+            entries.forEach((entry) => {
+                setIsVisible1(entry.isIntersecting);
+            });
+        };
+
+        const handleIntersect2: IntersectionObserverCallback = (entries) => {
+            entries.forEach((entry) => {
+                setIsVisible2(entry.isIntersecting);
+            });
+        };
+        const handleIntersect3: IntersectionObserverCallback = (entries) => {
+            entries.forEach((entry) => {
+                setIsVisible3(entry.isIntersecting);
+            });
+        };
+
+        const observer1 = new IntersectionObserver(handleIntersect1, options);
+        const observer2 = new IntersectionObserver(handleIntersect2, options);
+        const observer3 = new IntersectionObserver(handleIntersect3, options);
+
+        if (targetRef1.current) {
+            observer1.observe(targetRef1.current);
+        }
+
+        if (targetRef2.current) {
+            observer2.observe(targetRef2.current);
+        }
+
+        if (targetRef3.current) {
+            observer3.observe(targetRef3.current);
+        }
+
+        return () => {
+            observer1.disconnect();
+            observer2.disconnect();
+            observer3.disconnect();
+        };
+    }, []);
+        
+    return (
+        <div>
+            <Container>
+                <Header/>
+                <Container_main>
+                    <img src="./image/main_img.png" style={{ width: "1100px", height: "500px", marginTop:"70px"}}></img>
+                </Container_main>
+                <Container_1>
+                    <Container_1_page_1>
+                        <Container_1_page_1_element_1>
+                            Introduce 
+                        </Container_1_page_1_element_1>
+                        <Container_1_page_1_element_2>
+                            모두를 위한 비대면 계약 서비스
+                        </Container_1_page_1_element_2>
+                        <Container_1_page_1_element_3>
+                            바쁜 삶과 스케줄때문에 대면 계약을 하지 못하신다면 BLOCKER를 <br/> 추천합니다. '블록체인 '을 적용한 안전한 비대면 서비스로 당신의 시간을  <br/> 절약하세요.
+                        </Container_1_page_1_element_3>
+                        <Container_1_page_1_element_4>
+                            <img src="./image/enter_1.png" style={{ width: "30px", height: "30px", marginTop:"15px", marginLeft:"10px"}}></img>
+                            <div style={{marginTop:"20px" , marginLeft:"10px"}}>자세히 보기</div>
+                        </Container_1_page_1_element_4>
+                    </Container_1_page_1>
+                    <Container_1_page_2>
+                        <AnimatedBox1 ref={targetRef1} isVisible={isVisible1}>
+                            <img src="./image/temp_img.png" style={{ width: "600px", height: "360px", marginTop:"140px"}}></img>
+                        </AnimatedBox1>
+                    </Container_1_page_2>
+                </Container_1>
+                <Container_2>
+                    <Container_2_page_1>
+                        <AnimatedBox2 ref={targetRef2} isVisible={isVisible2}>
+                            <img src="./image/temp_img2.png" style={{ width: "600px", height: "340px"}}></img>
+                        </AnimatedBox2>
+                    </Container_2_page_1>
+                    <Container_2_page_2>
+                        <Container_2_page_2_element_1>
+                            SUPPORT
+                        </Container_2_page_2_element_1>
+                        <Container_2_page_2_element_2>
+                            WEB & APP 지원
+                        </Container_2_page_2_element_2>
+                        <Container_2_page_2_element_3>
+                            BLOCKER는 웹버전과 앱버전을 지원합니다. <br/> 다양한 디바이스를 통해 BLOCKER를 이용해보세요.
+                        </Container_2_page_2_element_3>
+                        <Container_2_page_2_element_4>
+                            <img src="./image/enter_1.png" style={{ width: "30px", height: "30px", marginTop:"15px", marginLeft:"10px"}}></img>
+                            <div style={{marginTop:"20px" , marginLeft:"10px"}}>자세히 보기</div>
+                        </Container_2_page_2_element_4>
+                    </Container_2_page_2>
+                </Container_2>
+                <Container_3>
+                    <Container_3_page_1>
+                        <Container_3_page_1_element_1>
+                            function 
+                        </Container_3_page_1_element_1>
+                        <Container_3_page_1_element_2>
+                            전자서명 & 실시간 채팅 & 분산원장 
+                        </Container_3_page_1_element_2>
+                        <Container_3_page_1_element_3>
+                            법적효력이 있는 전자서명과 실시간채팅 그리고 분산원장을 통한 <br/> 안전한 서비스등 다양한 기능을 BLOCKER를 통해 만나요.
+                        </Container_3_page_1_element_3>
+                        <Container_3_page_1_element_4>
+                            <img src="./image/enter_1.png" style={{ width: "30px", height: "30px", marginTop:"15px", marginLeft:"10px"}}></img>
+                            <div style={{marginTop:"20px" , marginLeft:"10px"}}>자세히 보기</div>
+                        </Container_3_page_1_element_4>
+                    </Container_3_page_1>
+                    <Container_3_page_2>
+                        <AnimatedBox3 ref={targetRef3} isVisible={isVisible3}>
+                            <img src="./image/temp_img.png" style={{ width: "600px", height: "360px", marginTop:"140px"}}></img>
+                        </AnimatedBox3>
+                    </Container_3_page_2>
+                </Container_3>
+            </Container>
+        </div>
+    );
+};
+
+const Container = styled.div`
+    // background : red;
+    position : relative;
+    height: fit-content;
+    width: 100%;
+`;
+const Container_main = styled.div`
+    position : relative;
+    background : #025CFB;
+    width : 100%;
+    height : 600px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+const Container_1 = styled.div`
+    position : relative;
+    // background : blue;
+    position : relative;
+    width : 1200px;
+    height : 650px;
+    left:50%;
+    transform : translate(-50%);
+    display : flex;
+`;
+const Container_1_page_1 = styled.div`
+    position : relative;
+    // background : green;
+    position : relative;
+    width : 600px;
+    height : 100%;
+    border-right: 2px dotted  #dfdfdf;
+`;
+const Container_1_page_1_element_1 = styled.div`
+    position : relative;
+    // background : blue;
+    position : relative;
+    width : fit-content;
+
+    display : flex;
+    justify-content : center;
+    align-items : center;
+
+    color : #000000;
+    font-size : 60px;
+    // font-weight : bold;
+    font-family: 'Rubik Mono One', sans-serif;
+    // text-shadow: 2px 2px 1px #04D0E7; /* 그림자 스타일 지정 */
+
+    margin-top : 165px;
+    margin-left : -3px;
+`;
+const Container_1_page_1_element_2 = styled.div`
+    position : relative;
+    // background : blue;
+    position : relative;
+    width : fit-content;
+
+    display : flex;
+    justify-content : center;
+    align-items : center;
+
+    color : #435DF1;
+    font-size : 17px;
+    font-weight : bold;
+
+    margin-top: 15px;
+`;
+const Container_1_page_1_element_3 = styled.div`
+    position : relative;
+    // background : blue;
+    position : relative;
+    width : fit-content;
+
+    display : flex;
+    justify-content : center;
+    align-items : center;
+    color : #A0A0A0;
+    font-size : 17px;
+    font-weight : bold;
+
+    margin-top: 15px;
+`;
+const Container_1_page_1_element_4 = styled.div`
+    position : relative;
+    // background : blue;
+    width : 150px;
+    height : 60px;
+
+    border : 1px solid #dfdfdf;
+    border-radius : 4px;
+    margin-top: 50px;
+
+    display : flex;
+    cursor : pointer;
+
+    &:hover {
+        background:#435DF1;
+        color : #ffffff;
+    }
+    &:hover img {
+        content: url('/image/enter_2.png'); /* 호버 시 이미지 변경 */
+    }
+`;
+const Container_1_page_2 = styled.div`
+    position : relative;
+    // background : brown;
+    position : relative;
+    width : 600px;
+    height : 100%;
+`;
+const Container_2 = styled.div`
+  position: relative;
+//   background: aqua;
+  position: relative;
+  width: 1200px;
+  height: 650px;
+  left: 50%;
+  transform: translate(-50%);
+  display : flex;
+`;
+const Container_2_page_1 = styled.div`
+    position : relative;
+    // background : green;
+    position : relative;
+    width : 599.5px;
+    height : 100%;
+    
+`;
+const Container_2_page_2 = styled.div`
+    position : relative;
+    // background : green;
+    position : relative;
+    width : 600.5px;
+    height : 100%;
+    border-left: 2px dotted  #dfdfdf;
+`;
+const Container_2_page_2_element_1 = styled.div`
+    position : relative;
+    // background : blue;
+    position : relative;
+    width : fit-content;
+
+    display : flex;
+    justify-content : center;
+    align-items : center;
+
+    color : #000000;
+    font-size : 60px;
+    // font-weight : bold;
+    font-family: 'Rubik Mono One', sans-serif;
+    // text-shadow: 2px 2px 1px #04D0E7; /* 그림자 스타일 지정 */
+
+    margin-top : 200px;
+    margin-left : 50px;
+`;
+const Container_2_page_2_element_2 = styled.div`
+    position : relative;
+    // background : blue;
+    position : relative;
+    width : fit-content;
+
+    display : flex;
+    justify-content : center;
+    align-items : center;
+
+    color : #435DF1;
+    font-size : 17px;
+    font-weight : bold;
+
+    margin-top: 15px;
+    margin-left : 53px;
+`;
+const Container_2_page_2_element_3 = styled.div`
+    position : relative;
+    // background : blue;
+    position : relative;
+    width : fit-content;
+
+    display : flex;
+    justify-content : center;
+    align-items : center;
+    color : #A0A0A0;
+    font-size : 17px;
+    font-weight : bold;
+
+    margin-top: 15px;
+    margin-left : 53px;
+`;
+const Container_2_page_2_element_4 = styled.div`
+    position : relative;
+    // background : blue;
+    width : 150px;
+    height : 60px;
+
+    border : 1px solid #dfdfdf;
+    border-radius : 4px;
+    margin-top: 50px;
+
+    display : flex;
+    cursor : pointer;
+    margin-left : 53px;
+
+    &:hover {
+        background:#435DF1;
+        color : #ffffff;
+    }
+    &:hover img {
+        content: url('/image/enter_2.png'); /* 호버 시 이미지 변경 */
+    }
+`;
+
+const Container_3 = styled.div`
+  position: relative;
+//   background: aqua;
+  position: relative;
+  width: 1200px;
+  height: 650px;
+  left: 50%;
+  transform: translate(-50%);
+  display : flex;
+`;
+const Container_3_page_1 = styled.div`
+    position : relative;
+    // background : green;
+    position : relative;
+    width : 599.5px;
+    height : 100%;
+    border-right: 2px dotted  #dfdfdf;
+`;
+const Container_3_page_1_element_1 = styled.div`
+    position : relative;
+    // background : blue;
+    position : relative;
+    width : fit-content;
+
+    display : flex;
+    justify-content : center;
+    align-items : center;
+
+    color : #000000;
+    font-size : 60px;
+    // font-weight : bold;
+    font-family: 'Rubik Mono One', sans-serif;
+    // text-shadow: 2px 2px 1px #04D0E7; /* 그림자 스타일 지정 */
+
+    margin-top : 165px;
+    margin-left : -3px;
+`;
+const Container_3_page_1_element_2 = styled.div`
+    position : relative;
+    // background : blue;
+    position : relative;
+    width : fit-content;
+
+    display : flex;
+    justify-content : center;
+    align-items : center;
+
+    color : #435DF1;
+    font-size : 17px;
+    font-weight : bold;
+
+    margin-top: 15px;
+`;
+const Container_3_page_1_element_3 = styled.div`
+    position : relative;
+    // background : blue;
+    position : relative;
+    width : fit-content;
+
+    display : flex;
+    justify-content : center;
+    align-items : center;
+    color : #A0A0A0;
+    font-size : 17px;
+    font-weight : bold;
+
+    margin-top: 15px;
+`;
+const Container_3_page_1_element_4 = styled.div`
+    position : relative;
+    // background : blue;
+    width : 150px;
+    height : 60px;
+
+    border : 1px solid #dfdfdf;
+    border-radius : 4px;
+    margin-top: 50px;
+
+    display : flex;
+    cursor : pointer;
+
+    &:hover {
+        background:#435DF1;
+        color : #ffffff;
+    }
+    &:hover img {
+        content: url('/image/enter_2.png'); /* 호버 시 이미지 변경 */
+    }
+`;
+const Container_3_page_2 = styled.div`
+    position : relative;
+    // background : brown;
+    position : relative;
+    width : 600px;
+    height : 100%;
+`;
+
+const AnimatedBox1 = styled.div<AnimatedBoxProps>`
+  width: 100%;
+  height:340px;
+//   background-color: blue;
+  position: absolute;
+  opacity: ${({ isVisible }) => (isVisible ? 1 : 0)};
+  transition: opacity 1s ease-in-out;
+//   margin-top : 170px;
+`;
+const AnimatedBox2 = styled.div<AnimatedBoxProps>`
+  width: 100%;
+  height:340px;
+//   background-color: blue;
+  position: absolute;
+  opacity: ${({ isVisible }) => (isVisible ? 1 : 0)};
+  transition: opacity 1s ease-in-out;
+  margin-top : 170px;
+`;
+const AnimatedBox3 = styled.div<AnimatedBoxProps>`
+  width: 100%;
+  height:340px;
+//   background-color: blue;
+  position: absolute;
+  opacity: ${({ isVisible }) => (isVisible ? 1 : 0)};
+  transition: opacity 1s ease-in-out;
+`;
+
+
+export default Main;
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,206 @@
+'use client'
+
+import React, { useState } from 'react';
+// import { Link, useNavigate } from "react-router-dom";
+import styled from 'styled-components';
+// import { deleteCookie } from "../utils/cookieUtils";
+// import Login from '../pages/Login';
+// import appStore from '../store/appStore';
+
+
+const Header = () => {
+    // const navigate = useNavigate();
+
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false); // dropdown 메뉴 상태
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(prev => !prev);
+    };
+
+    //쿠키삭제
+    const handleLogoutClick = () => {
+        // deleteCookie('쿠키이름');
+        
+        //쿠키삭제 이런코드
+        // navigate('/login');
+    };
+
+    /*로그인으로 이동하도록 하기*/
+    const handleGotoLogin = () => {
+        // appStore.setValue(2)
+    };
+    
+
+    return (
+        <Container>
+            {/* <StyledLink to="/" style={{ textDecoration: 'none' }}> */}
+                <Container_title>
+                    BLOCKER
+                </Container_title>
+            {/* </StyledLink> */}
+            <Container_menu>
+                
+
+                {/* <StyledLink to="/board" style={{ textDecoration: 'none' }}> */}
+                    <Container_menu_item>
+                        게시글
+                    </Container_menu_item>
+                {/* </StyledLink> */}
+                <Container_menu_item onClick={handleGotoLogin}> 
+                    게시글
+                </Container_menu_item>
+
+
+                {/* <StyledLink to="/postwrite" style={{ textDecoration: 'none' }}> */}
+                    <Container_menu_item>
+                        게시글작성
+                    </Container_menu_item>
+                {/* </StyledLink> */}
+                {/* <StyledLink to="/contracts" style={{ textDecoration: 'none' }}> */}
+                    <Container_menu_item>
+                        계약서/작성
+                    </Container_menu_item>
+                {/* </StyledLink> */}
+                {/* <StyledLink to="/vertification" style={{ textDecoration: 'none' }}> */}
+                    <Container_menu_item>
+                        검증
+                    </Container_menu_item>
+                {/* </StyledLink> */}
+            </Container_menu>
+            <Container_login>
+                <Container_login_btn  onClick={toggleDropdown}>
+                    <img src="../image/user.png" style={{ width: "23px", height: "23px"}}></img>
+                    <img src="../image/menu.png" style={{ width: "23px", height: "23px", marginLeft:"10px"}}></img>
+                    {isDropdownOpen &&         
+                    <DropdownContainer>
+                        {/* <StyledLink to="/mypage" style={{ textDecoration: 'none' }}> */}
+                            <DropdownItem>마이페이지</DropdownItem>
+                        {/* </StyledLink> */}
+                        <DropdownItem onClick={handleLogoutClick}>로그아웃</DropdownItem>
+                    </DropdownContainer>
+                    }
+                </Container_login_btn>     
+            </Container_login>
+
+        </Container>
+    );
+};
+
+
+const handleLogout = () => {
+    // 로그아웃 처리를 여기에 구현
+};
+// const StyledLink = styled(Link)`
+//   text-decoration: none;
+//   color: black; /* 원하는 색상으로 변경 */
+  
+//   &:visited {
+//     color: black; /* 방문한 링크의 색상도 동일하게 유지 */
+//   }
+// `;
+const Container = styled.div`
+    background : white;
+    position : fixed;
+    height: 80px;
+    width: 100%;
+    border-bottom: 1px solid #dfdfdf;
+    display : flex;
+    justify-content : space-between;
+    z-index : 1;
+`;
+const Container_title = styled.div`
+    // background : red;
+    width : 200px;
+    height : 100%;
+
+    font-size : 20px;
+    // font-weight : bold;
+
+    display : flex;
+    justify-content : center;
+    align-items : center;
+    font-family: 'Rubik Mono One', sans-serif;
+    // text-shadow: 2px 2px 4px #4CC6A9; /* 그림자 스타일 지정 */
+`;
+const Container_menu = styled.div`
+    // background : blue;
+    width : 500px;
+    height : 100%;
+    display : flex;
+    justify-content : space-evenly;
+    margin-left: 10px
+`;
+
+const Container_menu_item = styled.div`
+    padding: 10px;
+    width:100px;
+    height:60px;
+    cursor: pointer;
+    color : #000000;
+    font-weight:bold;
+    &:hover {
+        background-color: #dfdfdf;
+    }
+    
+    font-size : 13px;
+    display : flex;
+    justify-content : center;
+    align-items : center;
+`;
+const Container_login = styled.div`
+    // background : red;
+    width : 200px;
+    height : 30px;
+
+    font-size : 15px;
+    font-weight : bold;
+
+    display : flex;
+    justify-content : center;
+    align-items : center;
+
+    margin-top : 25px;
+
+`;
+const Container_login_btn = styled.div`
+    // background : aqua;
+    width : 100px;
+    height : 100%;
+
+    border-radius:5px;
+    font-size : 15px;
+    font-weight : bold;
+
+    display : flex;
+    justify-content : center;
+    align-items : center;
+    cursor : pointer;
+    margin-left:65px;
+`;
+const DropdownContainer = styled.div`
+    background : white;
+    position: absolute;
+    width : 98px;
+    right: 0;
+    border: 1px solid #dfdfdf;
+    box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);
+    z-index: 1;
+    margin-top : 105px;
+    margin-right:16px;
+`;
+
+const DropdownItem = styled.div`
+    padding: 10px;
+    cursor: pointer;
+    &:hover {
+        background-color: #dfdfdf;
+    }
+    
+    font-size : 13px;
+    display : flex;
+    justify-content : center;
+    align-items : center;
+`;
+
+
+export default Header;
