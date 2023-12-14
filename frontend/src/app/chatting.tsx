3e7a7d85268ddf3d979c2146207c306aa4096cc0
YOON-CC <whdbscks77@gmail.com>
2023-12-14 18:23:01 +0900

    feat(chattingList.tsx): stomp통신 구축, 이후 ui 변경 필요

--- frontend/src/app/chatting.tsx
+++ frontend/src/app/chatting.tsx
@@ -1,43 +1,76 @@
 'use client'
 
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
+import SockJS from 'sockjs-client';
+import Stomp from 'stompjs'; 
 import ChattingList from './chattingList';
 
-const Chatting = () => {
-    const [isChattingListVisible, setIsChattingListVisible] = useState(false);
+const Chatting: React.FC = () => {
+  const access_token = localStorage.getItem('access-token');
+  const [isVisible, setIsVisible] = useState(false);
+  let stompClient: Stomp.Client | null = null;
 
-    const toggleVisibility = () => {
-        setIsChattingListVisible(!isChattingListVisible);
-    };
 
-    return (
-        <div>
-            <Container onClick={toggleVisibility}>
-                <img src="./image/login_logo.png" style={{ width: "85%", height: "85%", marginLeft: '4px', marginTop: '3.5px' }}></img>
-            </Container>
-            {isChattingListVisible && <ChattingList />}
-        </div>
-    );
+  useEffect(() => {
+    if (isVisible) {
+      // Stomp 연결 설정
+      // const socket = new SockJS('http://43.202.127.236:8080/chat');
+      const socket = new WebSocket('ws://43.202.127.236:8080/chat');
+
+      stompClient = Stomp.over(socket);
+
+      const headers = {
+        Authorization: access_token, 
+      };
+
+      stompClient.connect(headers, (frame) => {
+        console.log('소켓 연결됨.');
+      }, (error) => {
+        console.log('Error: ' + error);
+      });
+
+      return () => {
+        // 컴포넌트 언마운트 시 정리
+        if (stompClient && stompClient.connected) {
+          stompClient.disconnect(() => {
+            console.log('Stomp 연결 해제됨');
+          });
+        }
+      };
+    }
+  }, [isVisible]);
+
+  const handleShowChattingList = () => {
+    setIsVisible(!isVisible);
+  };
+
+  return (
+    <div>
+      <Container onClick={handleShowChattingList}>
+        <img src="./image/login_logo.png" style={{ width: "85%", height: "85%", marginLeft: '4px', marginTop: '3.5px' }} alt="로고"></img>
+      </Container>
+      {isVisible && <ChattingList stompClient={stompClient} />}
+    </div>
+  );
 };
 
 const Container = styled.div`
-    background: white;
-    position: fixed;
-    width: 50px;
-    height: 50px;
-    margin-top: 100px;
-    top: 78%;
-    left: 96%;
-    transform: translate(-50%, -50%);
-    border-radius: 5px;
-    cursor: pointer;
-    border-radius: 5px;
-    box-shadow: 0px 1px 10px rgba(0, 0, 0, 0.4);
-    &:hover {
-        filter: brightness(0.9);
-    }
+  background: white;
+  position: fixed;
+  width: 50px;
+  height: 50px;
+  margin-top: 100px;
+  top: 78%;
+  left: 96%;
+  transform: translate(-50%, -50%);
+  border-radius: 5px;
+  cursor: pointer;
+  border-radius: 5px;
+  box-shadow: 0px 1px 10px rgba(0, 0, 0, 0.4);
+  &:hover {
+    filter: brightness(0.9);
+  }
 `;
-
 
 export default Chatting;
--- frontend/src/app/chattingList.tsx
+++ frontend/src/app/chattingList.tsx
@@ -1,56 +1,143 @@
-'use client'
-
+// 'use client'
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
+import Stomp from 'stompjs';
 
-const ChattingList = () => {
+interface ChattingListProps {
+  stompClient: React.RefObject<Stomp.Client> | null;
+}
 
-    const access_token = localStorage.getItem('access-token');
+interface ChattingList {
+  chatRoomId: number;
+  lastChat: number;
+  lastChatTime: number;
+}
 
-    const handleChattingList = async () => {
-        
-        try {
-            const response = await axios.get(`${process.env.NEXT_PUBLIC_REACT_APP_API_URL}/chatrooms`, {
-                headers: {
-                    'Authorization': access_token,
-                }
-            });
-            console.log(response.data)
+const ChattingList: React.FC<ChattingListProps> = ({ stompClient }) => {
+  const [chattingListData, setChattingListData] = useState<ChattingList[]>([]);
+  const access_token = localStorage.getItem('access-token') ?? '';
+  const socket = new WebSocket('ws://43.202.127.236:8080/chat');
+  let stompClientInstance: Stomp.Client | null = null;
 
-        } catch (error) {
+  stompClientInstance = Stomp.over(socket);
 
-        }
-    };
+  const headers = {
+    Authorization: access_token,
+  };
 
-    useEffect(() => {
-        handleChattingList();
-    }, []);
+  stompClientInstance.connect(headers, (frame) => {
+    console.log('소켓 연결됨.');
+  }, (error) => {
+    console.log('Error: ' + error);
+  });
 
+  const handleChattingList = async () => {
+    try {
+      const response = await axios.get(`${process.env.NEXT_PUBLIC_REACT_APP_API_URL}/chatrooms`, {
+        headers: {
+          'Authorization': access_token,
+        },
+      });
+      setChattingListData(response.data);
+    } catch (error) {
+      console.error('채팅방 목록을 불러오는 중 오류 발생:', error);
+    }
+  };
 
+  useEffect(() => {
+    handleChattingList();
+  }, []);
 
-    return (
-        <ListContainer>
+  const handleChatRoomClick = (chatRoomId: number) => {
+    console.log('채팅방 클릭');
+    if (stompClientInstance) {
+      const subscription = stompClientInstance.subscribe(`/sub/${chatRoomId}`, (message: { body: string }) => {
+        const chatMessages = document.getElementById('chatMessages') as HTMLUListElement;
+        const messageText = JSON.parse(message.body).content;
+        const sender = JSON.parse(message.body).sender;
+        const messageElement = document.createElement('li');
+        messageElement.textContent = sender + ': ' + messageText;
+        chatMessages.appendChild(messageElement);
+      }, {
+        Authorization: access_token,
+      });
 
-        </ListContainer>
-    );
+      return () => {
+        // 컴포넌트가 언마운트될 때 채널 구독 해제
+        subscription.unsubscribe();
+      };
+    }
+  };
+
+  const sendMessage = () => {
+    const messageInput = document.getElementById('messageInput') as HTMLInputElement;
+    const message = messageInput.value.trim();
+
+    if (!message) {
+      alert('메시지를 입력하세요.');
+      return;
+    }
+
+    if (stompClientInstance && stompClientInstance.connected) {
+      const currentRoomId = 9; // 실제로 사용하는 방법으로 대체
+      const headers = {
+        Authorization: access_token,
+      };
+
+      const chatMessage = {
+        content: message,
+      };
+
+      stompClientInstance.send(`/pub/message/${currentRoomId}`, headers, JSON.stringify(chatMessage));
+
+      messageInput.value = '';
+    } else {
+      alert('소켓에 연결되어 있지 않습니다.');
+    }
+  };
+
+  return (
+    <ListContainer>
+      {chattingListData.map((item, index) => (
+        <ListContainerFrame key={index} onClick={() => handleChatRoomClick(item.chatRoomId)}>
+          {item.chatRoomId}
+          {item.lastChat}
+          {item.lastChatTime}
+        </ListContainerFrame>
+      ))}
+      <div>
+        <input type="text" id="messageInput" placeholder="메시지를 입력하세요" />
+        <button onClick={sendMessage}>전송</button>
+        <ul id="chatMessages"></ul>
+      </div>
+    </ListContainer>
+  );
 };
 
 const ListContainer = styled.div`
-    background: white;
-    position: fixed;
-    width: 250px;
-    height: 400px;
-    margin-top: -130px;
-    margin-left : -100px;
-    top: 78%;
-    left: 96%;
-    transform: translate(-50%, -50%);
-    border-radius: 5px;
-    cursor: pointer;
-    border-radius: 5px;
-    box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.4);
+  background: white;
+  position: fixed;
+  width: 250px;
+  height: 400px;
+  margin-top: -130px;
+  margin-left: -100px;
+  top: 78%;
+  left: 96%;
+  transform: translate(-50%, -50%);
+  border-radius: 5px;
+  cursor: pointer;
+  border-radius: 5px;
+  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.4);
 `;
 
+const ListContainerFrame = styled.div`
+  background: #e3e3e3;
+  width: 240px;
+  height: 60px;
+  margin-top: 5px;
+  margin-left: 5px;
+  border-radius: 3px;
+`;
 
 export default ChattingList;
